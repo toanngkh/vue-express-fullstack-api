233b1bb37f621532071ad36182d9dddaccdc8b30
Toan Nguyen <toanngkh@gmail.com>
2019-01-01 17:13:37 -0600

    First

---
+++ server/routes/api/posts.js
@@ -0,0 +1,38 @@
+const express = require('express'); 
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+})
+
+// Add Post
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+})
+
+// Delete Post
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+})
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient
+                                .connect("mongodb://user1:abc123@ds147344.mlab.com:47344/vue-fullstack", {
+                                    useNewUrlParser: true
+                                });
+
+    return client.db('vue-fullstack').collection('posts');
+}
+
+module.exports = router;
